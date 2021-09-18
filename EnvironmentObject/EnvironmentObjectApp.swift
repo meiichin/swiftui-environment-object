b0ea768e86e52b9fb47c90f57b5ca0ea5b4a3677
meiichin <meiichan3017@gmail.com>
2021-09-18 17:03:51 +0700

    Initial Commit

---
+++ EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by Yuu on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
